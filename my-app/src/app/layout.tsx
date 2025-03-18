dc6292071c993d928b2d67b0238388581997dde5
smileeeeely <minjoole.ivy@gmail.com>
2025-03-18 16:59:24 +0900

    Update: Favicon 추가

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -6,6 +6,9 @@ import Provider from "./Provider";
 export const metadata: Metadata = {
   title: "리그 오브 레전드 정보 앱",
   description: "Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
 };
 
 export default function RootLayout({
